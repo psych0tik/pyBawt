a63c6126e8e9dfe104db0e98c2cc025addeaadf4
richo <richo@psych0tik.net>
2011-09-26 18:19:34 +1000

    Add colour
    (Also lol it colours my diffs :D )

--- test/unit/helpers.py
+++ test/unit/helpers.py
@@ -2,16 +2,37 @@
 import os
 import sys
 
+reset = "[39m"
+def red(text):
+    red = "[31m"
+    return "%s%s%s" % (red, text, reset)
+def green(text):
+    green = "[32m"
+    return "%s%s%s" % (green, text, reset)
+def yellow(text):
+    yellow = "[33m"
+    return "%s%s%s" % (yellow, text, reset)
+def blue(text):
+    blue = "[34m"
+    return "%s%s%s" % (blue, text, reset)
+def purple(text):
+    purple = "[35m"
+    return "%s%s%s" % (purple, text, reset)
+def cyan(text):
+    cyan = "[36m"
+    return "%s%s%s" % (cyan, text, reset)
+
+
 sys.path.append(os.getcwd())
 
 def ASSERT(cond, errormsg):
     if cond:
         print errormsg
-        print "\t\t\tPASS"
+        print green("\t\t\tPASS")
     else:
         # COLORS
         print >>sys.stderr, errormsg
-        print "\t\t\tFAIL"
+        print red("\t\t\tFAIL")
         # TODO Add this to something and continue
         # Maybe even raise a token exceptiont o catch so I can
         # examine the stack
@@ -19,9 +40,9 @@ def ASSERT(cond, errormsg):
 def START(test):
     # epic kludge
     os.current_test = test
-    print("== %s ==" % test)
+    print(cyan("== %s ==" % test))
 def END():
     try:
-        print("== %s ==" % os.current_test)
+        print(cyan("== %s ==" % os.current_test))
     except AttributeError:
-        ASSERT(False, "No test in progress")
+        ASSERT(False, red("No test in progress"))
