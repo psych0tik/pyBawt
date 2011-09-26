9b147763b8037a269120c80395894644a7a393eb
richo <richo@psych0tik.net>
2011-09-26 19:02:27 +1000

    This is an epic todo in and of itself

---
+++ stubs.py
@@ -0,0 +1,12 @@
+"""A loader interface to the stubs inside pyBawt's test harness, to make them
+available to the test suite"""
+
+# TODO
+
+class message(dict):
+    """for now just a thing that you can load more things into so much TODO"""
+   #def __getattribute__(self, key):
+   #    return self[key]
+   #def __setattribute__(self, key, value):
+   #    self[key] = value
+    pass
